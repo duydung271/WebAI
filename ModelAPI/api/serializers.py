aeb33fad4c7419d82cde9e0510f128c0b10072fd
duydung271 <ace02072001@gmail.com>
2021-10-22 00:13:23 +0700

    delete

--- ModelAPI/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ImageData
-
-class FileSerializer(serializers.ModelSerializer):
-  class Meta():
-    model = ImageData
-    fields = ('imageOrigin','imageBackground')
